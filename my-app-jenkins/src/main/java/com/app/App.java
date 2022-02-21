ff391ba5cf8905ef7b65fb37784e15ca97e8b7e6
AntoshaDevOps <96295455+AntoshaDevOps@users.noreply.github.com>
2022-02-22 00:12:52 +0300

    Update App.java

--- my-app-jenkins/src/main/java/com/app/App.java
+++ my-app-jenkins/src/main/java/com/app/App.java
@@ -8,8 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        int a = 0;
-        int b = 0;
+        //int a = 0;
+        //int b = 0;
         String newVar;
         System.out.println( "Hello World from Jenkins!" );
     }
